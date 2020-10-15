344a3025ba48460ea02249473d32e2a42993d283
shubhamurkude79 <shubhamurkude79@gmail.com>
2020-10-15 23:43:33 +0530

    Angular routing configured in the web app

--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CourseDetailComponent } from './course-detail/course-detail.component';
+import { CourseListComponent } from './course-list/course-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'course', component: CourseListComponent },
+  { path: 'course/:id', component: CourseDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/staticpages/staticpages-routing.module.ts
+++ src/app/staticpages/staticpages-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { PageComponent } from './page/page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'page/:slug', component: PageComponent },
+  { path: 'contact', component: ContactUsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
