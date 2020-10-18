50f85b9b92b93f40cfb197bbf5ab3562b2a5b33f
shubhamurkude79 <shubhamurkude79@gmail.com>
2020-10-18 23:31:46 +0530

    course-featured-component separated from course-list-component (both working individually)

--- src/app/courses/course-featured/course-featured.component.ts
+++ src/app/courses/course-featured/course-featured.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { CourseListComponent } from '../course-list/course-list.component';
+import { Component, OnInit } from '@angular/core';
 import { Course } from '../course.model';
 
 @Component({
@@ -8,8 +7,15 @@ import { Course } from '../course.model';
   styleUrls: ['./course-featured.component.css']
 })
 export class CourseFeaturedComponent implements OnInit {
-  @Input() course: Course;
-  @Input() courses: CourseListComponent;
+  
+  courses: Course[] = [
+    new Course('Angular 8 Full Course', 'Angular 8 is released in May 2019, just like it was scheduled. The new features of the framework are the updated form of Node 10..', 'assets/images/img1.png', 1),
+    new Course('Python Full Course', 'Python is one of the high-level general programming languages, interpreted, easy to use,complete and powerful. It is an object..', 'assets/images/img2.jpg', 2),
+    new Course('Angular Material course', 'Angular 8 is released in May 2019, just like it was scheduled. The new features of the framework are the updated form of Node 10', 'assets/images/img3.jpg', 3),
+    new Course('Entity Framework Course', 'Angular 8 is released in May 2019, just like it was scheduled. The new features of the framework are the updated form of Node 10', 'assets/images/img4.jpg', 4),
+    new Course('CSS Course', 'Angular 8 is released in May 2019, just like it was scheduled. The new features of the framework are the updated form of Node 10', 'assets/images/img5.jpg', 5),
+    new Course('TypeSript Course', 'Angular 8 is released in May 2019, just like it was scheduled. The new features of the framework are the updated form of Node 10', 'assets/images/img6.jpg', 6)
+  ];
 
   constructor() { }
 
